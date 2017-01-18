a33c3f7b0961dc6640cfb5be1a79e92e01cdf0cd
Robert <robert.lent@outlook.com>
2017-01-18 03:36:30 -0500

    Initial commit

---
+++ app/src/main/java/com/lentcoding/notes/EditorActivity.java
@@ -0,0 +1,140 @@
+package com.lentcoding.notes;
+
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class EditorActivity extends AppCompatActivity {
+
+    private String action;
+    private EditText editor;
+    private String noteFilter;
+    private String oldText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        editor = (EditText) findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getParcelableExtra(NotesProvider.CONTENT_ITEM_TYPE);
+
+        if (uri == null) {
+            action = Intent.ACTION_INSERT;
+            setTitle(getString(R.string.new_note));
+        } else {
+            action = Intent.ACTION_EDIT;
+            noteFilter = DBOpenHelper.NOTE_ID + " = " + uri.getLastPathSegment();
+
+            Cursor cursor = getContentResolver().query(uri, DBOpenHelper.ALL_COLUMNS, noteFilter, null, null);
+            cursor.moveToFirst();
+            oldText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+            editor.setText(oldText);
+            editor.requestFocus();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (action.equals(Intent.ACTION_EDIT)) {
+            getMenuInflater().inflate(R.menu.menu_editor, menu);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                finishEditing();
+                break;
+
+            case R.id.action_delete:
+                deleteNote();
+                break;
+        }
+
+        return true;
+    }
+
+    private void finishEditing() {
+        String newText = editor.getText().toString().trim();
+
+        switch (action) {
+            case Intent.ACTION_INSERT:
+                if (newText.length() == 0) {
+                    setResult(RESULT_CANCELED);
+                    finish();
+                } else {
+                    insertNote(newText);
+                    finish();
+                }
+                break;
+
+            case Intent.ACTION_EDIT:
+                if (oldText.equals(newText)) {
+                    setResult(RESULT_CANCELED);
+                    finish();
+                } else if (newText.length() == 0) {
+                    deleteNote();
+                } else {
+                    updateNote(newText);
+                    finish();
+                }
+                break;
+        }
+    }
+
+    private void deleteNote() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int button) {
+                if (button == DialogInterface.BUTTON_POSITIVE) {
+                    getContentResolver().delete(NotesProvider.CONTENT_URI, noteFilter, null);
+                    Toast.makeText(EditorActivity.this, getString(R.string.note_deleted), Toast.LENGTH_SHORT).show();
+                    setResult(RESULT_OK);
+                    finish();
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(R.string.no), dialogClickListener)
+                .show();
+    }
+
+    private void updateNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        getContentResolver().update(NotesProvider.CONTENT_URI, values, noteFilter, null);
+        Toast.makeText(this, R.string.note_updated, Toast.LENGTH_SHORT).show();
+        setResult(RESULT_OK);
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+        setResult(RESULT_OK);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishEditing();
+    }
+}
