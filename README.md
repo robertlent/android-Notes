1c8ae0db1b4e59be8cb22098a1cc9db3b3b37366
Robert <robert.lent@outlook.com>
2017-08-14 04:41:34 -0400

    Update App to v1.1.1
     - Add Screenshots
     - Add README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# Notes
+Notes is an app for taking notes.
+
+Users can add, edit, and delete notes. They also have the option of deleting all notes in the menu.
+
+### Screenshots
+![ScreenShot](/app/src/main/assets/Screenshots/AppInterfaceNotesList.png)
+![ScreenShot](/app/src/main/assets/Screenshots/AppInterfaceEdit.png)
+
+### Works on
+The app is will run on any device running Android 4.1 (Jelly Bean) or higher.
---
+++ app/src/main/java/com/lentcoding/notes/NotesCursorAdapterFragment.java
@@ -0,0 +1,41 @@
+package com.lentcoding.notes;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+@SuppressWarnings("SameParameterValue")
+class NotesCursorAdapterFragment extends CursorAdapter {
+    NotesCursorAdapterFragment(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.note_list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        String listItem, noteSubject, noteText;
+        noteSubject = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_SUBJECT));
+        noteText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+
+        if (noteSubject.length() == 0) {
+            listItem = noteText;
+        } else {
+            listItem = noteSubject;
+        }
+        int pos = listItem.indexOf(10);
+        if (pos != -1) {
+            listItem = listItem.substring(0, pos) + "...";
+        }
+
+        TextView tv = (TextView) view.findViewById(R.id.tvNote);
+        tv.setText(listItem);
+    }
+}
